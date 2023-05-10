725e44566dc6088a605890b3eb9f94e1d14168c1
Dhwani Choubisa <Dhwani.choubisa24@gmail.com>
2023-05-10 12:13:57 -0400

    Update tablefilter.js

--- html-report/js/tablefilter.js
+++ html-report/js/tablefilter.js
@@ -1,42 +1,30 @@
- <script>
-    $(document).ready(function() {
-      $('th.filterable').on('click', function() {
-        // Remove any existing dropdown menus
-        $('.filter-dropdown').remove();
-        
-        var columnIndex = $(this).index();
-        var columnText = $(this).text().trim();
-        var placeholder = $(this).find('.dropdown-placeholder');
-        
-        // Generate the dropdown menu
-        var dropdown = $('<select class="filter-dropdown">');
-        dropdown.append('<option value="">All</option>');
-        
-        // Extract unique values from the column cells
-        var values = [];
-        $('table tbody tr').each(function() {
-          var cellValue = $(this).find('td').eq(columnIndex).text().trim();
-          if (!values.includes(cellValue)) {
-            values.push(cellValue);
-            dropdown.append('<option value="' + cellValue + '">' + cellValue + '</option>');
-          }
-        });
-        
-        // Append the dropdown menu to the column header
-        var dropdownContainer = $('<div class="dropdown-menu">');
-        dropdownContainer.append(dropdown);
-        $(this).append(dropdownContainer);
-        placeholder.text(' ▼');
-      });
+<script>
+  $(document).ready(function() {
+    $('th.filterable').on('click', function(e) {
+      e.stopPropagation(); // Prevent the event from bubbling up
       
-      // Apply filtering when selecting an option from the dropdown menu
-      $(document).on('change', '.filter-dropdown', function() {
-        var columnIndex = $(this).closest('th').index();
-        var selectedValue = $(this).val().toLowerCase();
-        $('table tbody tr').each(function() {
-          var cellValue = $(this).find('td').eq(columnIndex).text().toLowerCase();
-          $(this).toggle(selectedValue === '' || cellValue === selectedValue);
-        });
+      var dropdown = $(this).find('.filter-dropdown');
+      dropdown.toggle();
+    });
+    
+    // Hide dropdown menus when clicking outside
+    $(document).on('click', function() {
+      $('.filter-dropdown').hide();
+    });
+    
+    // Prevent hiding the dropdown menu when clicking inside it
+    $(document).on('click', '.filter-dropdown', function(e) {
+      e.stopPropagation();
+    });
+    
+    // Apply filtering when selecting an option from the dropdown menu
+    $(document).on('change', '.filter-dropdown', function() {
+      var columnIndex = $(this).closest('th').index();
+      var selectedValue = $(this).val().toLowerCase();
+      $('table tbody tr').each(function() {
+        var cellValue = $(this).find('td').eq(columnIndex).text().toLowerCase();
+        $(this).toggle(selectedValue === '' || cellValue === selectedValue);
       });
     });
-  </script>
+  });
+</script>
