34a656446fcc05e6f7068374a5f944cfca2f7213
Dhwani Choubisa <Dhwani.choubisa24@gmail.com>
2023-05-10 11:55:48 -0400

    Update tablefilter.js

--- html-report/js/tablefilter.js
+++ html-report/js/tablefilter.js
@@ -1,42 +1,42 @@
-<script>
-  $(document).ready(function() {
-    $('th.filterable').on('click', function() {
-      // Remove any existing dropdown menus
-      $('.dropdown-menu').remove();
-      
-      var columnIndex = $(this).index();
-      var columnText = $(this).text().trim();
-      var placeholder = $(this).find('.dropdown-placeholder');
-      
-      // Generate the dropdown menu
-      var dropdown = $('<select class="filter-dropdown">');
-      dropdown.append('<option value="">All</option>');
-      
-      // Extract unique values from the column cells
-      var values = [];
-      $('table tbody tr').each(function() {
-        var cellValue = $(this).find('td').eq(columnIndex).text().trim();
-        if (!values.includes(cellValue)) {
-          values.push(cellValue);
-          dropdown.append('<option value="' + cellValue + '">' + cellValue + '</option>');
-        }
+ <script>
+    $(document).ready(function() {
+      $('th.filterable').on('click', function() {
+        // Remove any existing dropdown menus
+        $('.filter-dropdown').remove();
+        
+        var columnIndex = $(this).index();
+        var columnText = $(this).text().trim();
+        var placeholder = $(this).find('.dropdown-placeholder');
+        
+        // Generate the dropdown menu
+        var dropdown = $('<select class="filter-dropdown">');
+        dropdown.append('<option value="">All</option>');
+        
+        // Extract unique values from the column cells
+        var values = [];
+        $('table tbody tr').each(function() {
+          var cellValue = $(this).find('td').eq(columnIndex).text().trim();
+          if (!values.includes(cellValue)) {
+            values.push(cellValue);
+            dropdown.append('<option value="' + cellValue + '">' + cellValue + '</option>');
+          }
+        });
+        
+        // Append the dropdown menu to the column header
+        var dropdownContainer = $('<div class="dropdown-menu">');
+        dropdownContainer.append(dropdown);
+        $(this).append(dropdownContainer);
+        placeholder.text(' ▼');
       });
       
-      // Append the dropdown menu to the column header
-      var dropdownContainer = $('<div class="dropdown-menu">');
-      dropdownContainer.append(dropdown);
-      $(this).append(dropdownContainer);
-      placeholder.text(' ▼');
-    });
-    
-    // Apply filtering when selecting an option from the dropdown menu
-    $(document).on('change', '.filter-dropdown', function() {
-      var columnIndex = $(this).closest('th').index();
-      var selectedValue = $(this).val().toLowerCase();
-      $('table tbody tr').each(function() {
-        var cellValue = $(this).find('td').eq(columnIndex).text().toLowerCase();
-        $(this).toggle(selectedValue === '' || cellValue === selectedValue);
+      // Apply filtering when selecting an option from the dropdown menu
+      $(document).on('change', '.filter-dropdown', function() {
+        var columnIndex = $(this).closest('th').index();
+        var selectedValue = $(this).val().toLowerCase();
+        $('table tbody tr').each(function() {
+          var cellValue = $(this).find('td').eq(columnIndex).text().toLowerCase();
+          $(this).toggle(selectedValue === '' || cellValue === selectedValue);
+        });
       });
     });
-  });
-</script>
+  </script>
