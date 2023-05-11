6f0634f837c41276122d06e0f4241e51562fe924
Dhwani Choubisa <Dhwani.choubisa24@gmail.com>
2023-05-11 10:52:46 -0400

    Update tablefilter.js

--- html-report/js/tablefilter.js
+++ html-report/js/tablefilter.js
@@ -1,69 +1,29 @@
-$(document).ready(function() {
-  var table = $('#example').DataTable({
-    // Enable pagination
-    paging: true,
+(document).ready(function() {
+      var table = $('#myTable').DataTable({
+        drawCallback: function() {
+          // Reattach the change event handler to the filter dropdowns
+          $('.filter-dropdown').off('change').on('change', function() {
+            var columnIndex = $(this).closest('th').index();
+            var selectedValue = $(this).val().toLowerCase();
 
-    // Add the dropdowns to the header cells
-    initComplete: function() {
-      this.api().columns('.filterable').every(function() {
-        var column = this;
-        var columnIndex = column.index();
-        var header = $(column.header());
+            table.column(columnIndex).search(selectedValue).draw();
+          });
+        }
+      });
 
-        // Create the dropdown container
+      // Add the filter dropdowns to the header cells
+      $('.filterable').each(function() {
+        var columnIndex = $(this).index();
         var dropdownContainer = $('<div class="dropdown-container">');
-        header.append(dropdownContainer);
-
-        // Create the dropdown button
         var dropdownButton = $('<div class="dropdown-button">▼</div>');
-        dropdownContainer.append(dropdownButton);
-
-        // Create the dropdown
         var dropdown = $('<select class="filter-dropdown">');
         dropdown.append('<option value="">All</option>');
 
         // Populate the dropdown with unique values from the column
-        column.data().unique().sort().each(function(value) {
-          if (value !== '') {
-            dropdown.append('<option value="' + value + '">' + value + '</option>');
-          }
+        table.column(columnIndex).data().unique().sort().each(function(value) {
+          dropdown.append('<option value="' + value + '">' + value + '</option>');
         });
+
+        dropdownContainer.append(dropdownButton);
         dropdownContainer.append(dropdown);
-
-        // Apply filtering when selecting an option from the dropdown menu
-        dropdown.on('change', function() {
-          var selectedValue = $(this).val().toLowerCase();
-          column.search(selectedValue).draw();
-        });
-
-        // Hide dropdown menus when clicking outside
-        $(document).on('click', function() {
-          dropdownContainer.removeClass('active');
-        });
-
-        // Show/hide dropdown menus when clicking on the dropdown button
-        dropdownButton.on('click', function(e) {
-          e.stopPropagation(); // Prevent the event from bubbling up
-          dropdownContainer.toggleClass('active');
-        });
-
-        // Prevent hiding the dropdown menu when clicking inside it
-        dropdownContainer.on('click', function(e) {
-          e.stopPropagation();
-        });
-      });
-    },
-
-    // Callback function to keep the filter dropdowns visible after pagination
-    drawCallback: function() {
-      $('.dropdown-container.active').each(function() {
-        var columnIndex = $(this).closest('th').index();
-        var selectedValue = $(this).find('.filter-dropdown').val();
-        var column = table.column(columnIndex);
-
-        // Apply the filter to the new table data
-        column.search(selectedValue).draw();
-      });
-    }
-  });
-});
+        $(this).append(dropdownContainer);
